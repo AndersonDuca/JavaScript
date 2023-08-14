dd3af3f4c6362fbdb01fc0f6347831fb0430e757
AndersonDuca <133819508+AndersonDuca@users.noreply.github.com>
2023-08-14 13:01:26 -0300

    exercício 12 js

---
+++ aula12ex/ex014/script.js
@@ -0,0 +1,23 @@
+function  carregar() {
+    var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('imagem')
+var data = new Date()
+var hora =  20//data.getHours()
+msg.innerHTML = `Agora são ${hora} horas`
+if (hora >= 0 && hora < 12) {
+    // BOM DIA !
+    img.src = 'fotomanha.png'
+    document.body.style.background ='#ffead2'
+} else if (hora >= 12 && hora <=18 ) {
+    //BOA TARDE !
+    img.src = 'fototarde.png'
+    saldação.innerHTML = 'BOA TARDE!'
+    document.body.style.background ='#f09465'
+} else {
+    // BOA NOITE !
+    img.src = 'fotonoite.png'
+    document.body.style.background ='#030313'
+    saldação.innerHTML = 'BOA NOITE!'
+}
+}
+
